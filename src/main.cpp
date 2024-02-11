cc80b7a94c1fdcd2e57ef56f13a61330e1aedd37
Jack Whatley <wickselm2005@gmail.com>
2024-02-11 15:15:30 +0000

    Implementing Glad + OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 int main()
@@ -14,13 +15,19 @@ int main()
 
 	if (window == NULL)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
+		std::cout << "Failed to create GLFW window" << "\n";
 		glfwTerminate();
 
 		return -1;
 	}
 
 	glfwMakeContextCurrent(window);
+	gladLoadGL();
+
+	glViewport(0, 0, 800, 800);
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
 
 	while (!glfwWindowShouldClose(window))
 	{
